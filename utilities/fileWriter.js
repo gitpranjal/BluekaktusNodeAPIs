9ba8bbea07934fd2a819194af17617ac826fe636
gitpranjal <upranjal22@gmail.com>
2021-01-18 09:55:08 +0530

    added file writer

--- utilities/fileWriter.js
+++ utilities/fileWriter.js
@@ -1,55 +1,63 @@
 const fs = require('fs');
-var fieldList = ["name", "phone", "email-id"]
+
+var ObjectFromAPI = {
+      textFileds: ["name", "email", "phone", "gst", "adhaar", "JailNo"],
+      dropDowns: [],
+      functions: `{"frameSentence": (FieldsObject) => {
+        var sentence = ""
+        for(var key of Object.keys(FieldsObject))
+        {
+          sentence = sentence+key.toString()+":"+FieldsObject[key].toString()+"\\n"
+        }
+        return sentence
+      }}`
+}
+
+var fieldObjectList = {}
+for(var key of ObjectFromAPI.textFileds)
+  fieldObjectList[key] = ""
+
 let lyrics = 
 `
 import React, { useState } from "react";
 import {StyleSheet,Text,TextInput,View, TouchableOpacity, FlatList} from "react-native";
 
+const screenFunctions = ${ObjectFromAPI.functions}
 const GeneratedCode = () => {
-  const [Name, SetName] = useState("")
   const [Sentence, SetSentence] = useState("")
-  const [FieldList, SetFieldList] = useState(${fieldList})
+  const [FieldList, SetFieldList] = useState(${JSON.stringify(fieldObjectList)})
+
   return (
-    <View>
-    // <FlatList 
-    //     data={FieldList}
-    //     keyExtractor={(fieldName) => fieldName.toString()}
-    //     renderItem={({item}) => {
-    //     return (
+    <View style={{marginHorizontal: 10}}>
+     <FlatList 
+         data={Object.keys(FieldList)}
+         keyExtractor={(fieldName) => fieldName.toString()}
+         renderItem={({item}) => {
+         return (
         
 
-    //         <TextInput
-    //         // style= {{marginLeft: 4, color: "blue"}}
-    //         style={styles.input}
-    //         placeholder={"Name"}
-    //         placeholderTextColor={"grey"}
-    //         maxLength={50}
-    //         // onBlur={Keyboard.dismiss}
-    //         value={Name}
-    //         onChangeText = {(newName) => {
-    //             SetName(newName)
-    //         }}
-    //     />
-    //     )
-    //     }}
-    // /> 
-    <TextInput
-        // style= {{marginLeft: 4, color: "blue"}}
-        style={styles.input}
-        placeholder={"Name"}
-        placeholderTextColor={"grey"}
-        maxLength={50}
-        // onBlur={Keyboard.dismiss}
-        value={Name}
-        onChangeText = {(newName) => {
-            SetName(newName)
-        }}
-    />
-
+             <TextInput
+             // style= {{marginLeft: 4, color: "blue"}}
+             style={styles.input}
+              placeholder={item.toLowerCase()}
+             placeholderTextColor={"grey"}
+             maxLength={50}
+             // onBlur={Keyboard.dismiss}
+             value={FieldList.item}
+             onChangeText = {(newValue) => {
+                 var newFieldsObject = {...FieldList}
+                 newFieldsObject[item] = newValue
+                 SetFieldList(newFieldsObject)
+             }}
+         />
+         )
+         }}
+     /> 
+    
         <TouchableOpacity
         style={{ ...styles.openButton, marginHorizontal: 10, width: "20%", marginVertical: 10}}
         onPress={() => {
-            var newSentence = Name+" is a good boy"
+            var newSentence = screenFunctions.frameSentence(FieldList)
             SetSentence(newSentence)
         }}
         >
@@ -74,7 +82,7 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: "bold",
         borderRadius: 10,
-        width: "100%",
+        width: "100%"
        
        
       },
@@ -97,7 +105,7 @@ export default GeneratedCode;
 `;
 
 // write to a new file named 2pac.txt
-fs.writeFile('/home/zadmin/Desktop/BluekaktusReactNativeScreens/src/screens/GeneratedCode.js', lyrics, (err) => {
+fs.writeFile('/Users/geuser/Desktop/BluekaktusReactNativeScreens/src/screens/GeneratedCode.js', lyrics, (err) => {
     // throws an error, you could also catch it here
     if (err) throw err;
 
