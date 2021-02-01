d7715221ab93a67fa17aa121c3ac76f8ec40fa17
Pranjal Upadhyay <pranjal.upadhyay@bluekaktus.com>
2021-02-02 00:13:47 +0530

    corrected file path error and added logs for error diagnosis

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const multer = require('multer')
 const path = require('path')
 const bodyParser = require("body-parser")
 const helpers = require('./utilities/helpers');
+var moment = require('moment')
 
 const hostname = "localhost"
 const port = 3000
 
-const { getContourImage } = require("./utilities/effects")
+const { getContourImage } = require("./utilities/effects");
+const e = require("express");
 
 
 const storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, 'static/ImageUploads/');
+        cb(null, path.join(__dirname, 'static/ImageUploads/'));
     },
 
     // By default, multer removes file extensions so let's add them back
@@ -50,33 +52,45 @@ app.post("/getContourImage", (request, response) => {
             return response.send(request.fileValidationError);
         }
         else if (!request.file) {
+        {
+            console.log("######## File not found ############")
+            console.log(`Date and time : ${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
             return response.send('Please select an image to upload');
         }
+        }
         else if (err instanceof multer.MulterError) {
+            console.log("######## Multer error in file upload #######")
+            console.log(`Date and time : ${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
+            console.log(err)
             return response.send(err);
         }
         else if (err) {
+            console.log("########### Error in file upload #########")
+            console.log(`Date and time : ${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
+            console.log(err)
             return response.send(err);
         }
 
         
-        // const ImgUri =  `http://${hostname}:${port}/${request.file.path}`
-        const ImgUri =  `http://ai.bluekaktus.com/api/contourApi/${request.file.path}`
+        const ImgUri =  `http://${hostname}:${port}/static/ImageUploads/${request.file.path.split("ImageUploads/")[1]}`
+        // const ImgUri =  `http://ai.bluekaktus.com/api/contourApi/${request.file.path}`
         
 
         getContourImage(ImgUri, request.file.filename)
         .then(() => {
             console.log("###### Contour image generated and saved #########")
+            console.log(`Date and time: ${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
             result = {
                 "orignal": ImgUri,
-                "contour": `http://ai.bluekaktus.com/api/contourApi/static/contourImages/${request.file.filename}`
-                // "contour": `http://${hostname}:${port}/static/contourImages/${request.file.filename}`
+                // "contour": `http://ai.bluekaktus.com/api/contourApi/static/contourImages/${request.file.filename}`
+                "contour": `http://${hostname}:${port}/static/contourImages/${request.file.filename}`
             }
             console.log(result)
             response.json(result)
         })
         .catch((e) => {
             console.log("######## Countour imgage url sending error #######")
+            console.log(`Date and time: ${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
             console.log(e)
         })
         // result = {
